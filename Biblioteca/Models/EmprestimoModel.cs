acfce17303194b254b126163832514e77eb58031
JoaoAntonioGomesDSAMS <joaoantoniogms938@gmail.com>
2025-04-03 14:52:52 -0300

    commit para ajustar as pastas da biblioteca

---
+++ Biblioteca/Models/EmprestimoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Biblioteca.Models
+
+
+{
+    public class EmprestimoModel
+    {
+        public int Id { get; set; }
+        string Recebedor { get; set; }
+        string Fornecedor { get; set; }
+        string LivroEmprestado { get; set; }
+        DateTime DataEmprestimo { get; set; } = DateTime.Now;
+    }
+}
