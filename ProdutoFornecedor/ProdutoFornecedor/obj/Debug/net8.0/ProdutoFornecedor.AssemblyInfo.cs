d43258f9843ecbb63946e672de6ee5fb03eae954
JoaoAntonioGomesDSAMS <joaoantoniogms938@gmail.com>
2025-04-29 17:30:33 -0300

    sei la

--- ProdutoFornecedor/ProdutoFornecedor/obj/Debug/net8.0/ProdutoFornecedor.AssemblyInfo.cs
+++ ProdutoFornecedor/ProdutoFornecedor/obj/Debug/net8.0/ProdutoFornecedor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProdutoFornecedor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a104f984fdb71f2a795dff0181c84ecb7bf85f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProdutoFornecedor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProdutoFornecedor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
